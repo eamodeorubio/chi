5f370913fa9c0c9bab808ff75c45847d723aba6f
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-13 20:47:48 +0200

    Refactored test structure and descriptions to be more readable

--- test/core/emitter.js
+++ test/core/emitter.js
@@ -4,57 +4,59 @@ var expect = require('expect.js'),
     doubles = require('../doubles'),
     chi = require('../../lib/chi');
 
-describe('An Emitter', function () {
-  var anEmmitter;
+describe('An Emitter can yield values', function () {
+  var anEmmitter, value = "yielded value";
 
   beforeEach(function () {
     anEmmitter = chi.emitter();
   });
 
-  describe('can yield values', function () {
-    it('has a yield() method', function () {
-      expect(anEmmitter.yield).to.be.a('function');
+  it('it has a yield() method', function () {
+    expect(anEmmitter.yield).to.be.a('function');
+  });
+
+  describe('given it is chained to several feeds', function () {
+    var feed1, feed2;
+
+    beforeEach(function () {
+      feed1 = doubles.makeFeed();
+      feed2 = doubles.makeFeed();
+
+      anEmmitter.chain(feed1);
+      anEmmitter.chain(feed2);
     });
 
-    describe('given it is chained to several feeds', function () {
-      var feed1, feed2, feed3, value = "yielded value";
-
+    describe('when yield is invoked with a value, it', function () {
       beforeEach(function () {
-        feed1 = doubles.makeFeed();
-        feed2 = doubles.makeFeed();
-        feed3 = doubles.makeFeed();
-
-        anEmmitter.chain(feed1);
-        anEmmitter.chain(feed2);
-        anEmmitter.chain(feed3);
-      });
-
-      describe('when yield is invoked with a value, it', function () {
-        beforeEach(function () {
-          anEmmitter.yield(value);
-        });
-
-        it('will call yield on all chained feeds exactly one time', function () {
-          expect(feed1.yield.calledOnce).to.be.ok();
-          expect(feed2.yield.calledOnce).to.be.ok();
-          expect(feed3.yield.calledOnce).to.be.ok();
-        });
-
-        it('will pass the same argument to all chained feeds', function () {
-          expect(feed1.yield.calledWithExactly(value)).to.be.ok();
-          expect(feed2.yield.calledWithExactly(value)).to.be.ok();
-          expect(feed3.yield.calledWithExactly(value)).to.be.ok();
-        });
-      });
-
-      it('and a feed is chained several times, when yield is invoked with a value, it will still perform exactly one call', function () {
-        anEmmitter.chain(feed1);
-        anEmmitter.chain(feed1);
-
         anEmmitter.yield(value);
-
-        expect(feed1.yield.calledOnce).to.be.ok();
       });
+
+      it('will call yield on all chained feeds exactly one time', function () {
+        expect(feed1.yield.calledOnce).to.be.ok();
+        expect(feed2.yield.calledOnce).to.be.ok();
+      });
+
+      it('will pass the same argument to all chained feeds', function () {
+        expect(feed1.yield.calledWithExactly(value)).to.be.ok();
+        expect(feed2.yield.calledWithExactly(value)).to.be.ok();
+      });
+    });
+  });
+
+  describe('given it is chained to a feed several times', function () {
+    var aFeed;
+    beforeEach(function () {
+      aFeed = doubles.makeFeed();
+
+      anEmmitter.chain(aFeed);
+      anEmmitter.chain(aFeed);
+      anEmmitter.chain(aFeed);
+    });
+
+    it('when yield is invoked, it will still call yield on the chained feed exactly one time', function () {
+      anEmmitter.yield(value);
+
+      expect(aFeed.yield.calledOnce).to.be.ok();
     });
   });
 });

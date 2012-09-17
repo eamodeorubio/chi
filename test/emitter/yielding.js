74f47987e1ad67b53e744565e5a3a77be56d4a84
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-17 19:30:59 +0200

    More on yielding: yield will return the emitter

--- test/emitter/yielding.js
+++ test/emitter/yielding.js
@@ -27,10 +27,16 @@ describe('An Emitter can yield values:', function () {
     });
 
     describe('when yield is invoked with a value, it', function () {
+      var result;
+
       beforeEach(function () {
-        anEmmitter.yield(value);
+        result = anEmmitter.yield(value);
       });
 
+      it('will return itself', function () {
+        expect(result).to.be(anEmmitter);
+      });
+      
       it('will call yield on all chained feeds exactly one time', function () {
         expect(feed1.yield.calledOnce).to.be.ok();
         expect(feed2.yield.calledOnce).to.be.ok();

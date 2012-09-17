4f42ced7e99601f9ab12960c065bee5e3eaeae35
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-17 20:53:02 +0200

    Merged chaining and yielding files

--- test/emitter/chaining.js
+++ test/emitter/chaining.js
@@ -5,7 +5,7 @@ var expect = require('expect.js'),
     utils = require('../../lib/internal/utils'),
     chi = require('../../lib/chi');
 
-describe('An Emitter can be chained:', function () {
+describe('An Emitter:', function () {
   var anEmitter, originalEventBus, aBus;
 
   beforeEach(function () {
@@ -22,68 +22,96 @@ describe('An Emitter can be chained:', function () {
     utils.EventBus = originalEventBus;
   });
 
-  it('it has a chain() method', function () {
-    expect(anEmitter.chain).to.be.a('function');
+  describe('can be chained:', function () {
+    it('it has a chain() method', function () {
+      expect(anEmitter.chain).to.be.a('function');
+    });
+
+    describe('when chain is invoked with another feed,', function () {
+      var aFeed, chainResult;
+      beforeEach(function () {
+        aFeed = doubles.makeFeed();
+
+        chainResult = anEmitter.chain(aFeed);
+      });
+
+      it('will subscribe the feed to the bus', function () {
+        expect(aBus.subscribe.calledOnce).to.be.ok();
+        expect(aBus.subscribe.calledOn(aBus)).to.be.ok();
+        expect(aBus.subscribe.calledWithExactly(aFeed)).to.be.ok();
+      });
+
+      it('it will return a non null object', function () {
+        expect(chainResult).to.be.an('object');
+        expect(chainResult).not.to.be(null);
+      });
+
+      it('the object returned will have a chain() method', function () {
+        expect(chainResult.chain).to.be.a('function');
+      });
+
+      describe("the returned object's chain() method will call the chained feed's chain() method", function () {
+        var result, expectedResult, expectedArgument;
+
+        beforeEach(function () {
+          expectedArgument = 'some argument';
+          expectedResult = "returned value from chained feed's chain() method";
+
+          aFeed.chain.returns(expectedResult);
+
+          result = chainResult.chain(expectedArgument);
+        });
+
+        it('only once', function () {
+          expect(aFeed.chain.calledOnce).to.be.ok();
+        });
+
+        it('using the chained feed as the runtime context', function () {
+          expect(aFeed.chain.calledOn(aFeed)).to.be.ok();
+        });
+
+        it('using the same argument passed', function () {
+          expect(aFeed.chain.calledWithExactly(expectedArgument)).to.be.ok();
+        });
+
+        it('and will return the resulting value', function () {
+          expect(result).to.be(expectedResult);
+        });
+      });
+    });
+
+    it('chain() will throw if bus.subscribe() throws', function () {
+      aBus.subscribe.throws();
+
+      expect(function () {
+        anEmitter.chain("some object");
+      }).to.throwError();
+    });
   });
 
-  describe('when chain is invoked with another feed,', function () {
-    var aFeed, chainResult;
-    beforeEach(function () {
-      aFeed = doubles.makeFeed();
+  describe('can yield values:', function () {
+    var value = "yielded value";
 
-      chainResult = anEmitter.chain(aFeed);
+    it('it has a yield() method', function () {
+      expect(anEmitter.yield).to.be.a('function');
     });
 
-    it('will subscribe the feed to the bus', function () {
-      expect(aBus.subscribe.calledOnce).to.be.ok();
-      expect(aBus.subscribe.calledOn(aBus)).to.be.ok();
-      expect(aBus.subscribe.calledWithExactly(aFeed)).to.be.ok();
-    });
-
-    it('it will return a non null object', function () {
-      expect(chainResult).to.be.an('object');
-      expect(chainResult).not.to.be(null);
-    });
-
-    it('the object returned will have a chain() method', function () {
-      expect(chainResult.chain).to.be.a('function');
-    });
-
-    describe("the returned object's chain() method will call the chained feed's chain() method", function () {
-      var result, expectedResult, expectedArgument;
+    describe('when yield is invoked with a value, it', function () {
+      var result;
 
       beforeEach(function () {
-        expectedArgument = 'some argument';
-        expectedResult = "returned value from chained feed's chain() method";
-
-        aFeed.chain.returns(expectedResult);
-
-        result = chainResult.chain(expectedArgument);
+        result = anEmitter.yield(value);
       });
 
-      it('only once', function () {
-        expect(aFeed.chain.calledOnce).to.be.ok();
+      it('will return itself', function () {
+        expect(result).to.be(anEmitter);
       });
 
-      it('using the chained feed as the runtime context', function () {
-        expect(aFeed.chain.calledOn(aFeed)).to.be.ok();
-      });
-
-      it('using the same argument passed', function () {
-        expect(aFeed.chain.calledWithExactly(expectedArgument)).to.be.ok();
-      });
-
-      it('and will return the resulting value', function () {
-        expect(result).to.be(expectedResult);
+      it('will call publishYield on the bus with the yielded value', function () {
+        expect(aBus.publishYield.calledOnce).to.be.ok();
+        expect(aBus.publishYield.calledOn(aBus)).to.be.ok();
+        expect(aBus.publishYield.calledWithExactly(value)).to.be.ok();
       });
     });
-  });
-
-  it('chain() will throw if bus.subscribe() throws', function () {
-    aBus.subscribe.throws();
-
-    expect(function () {
-      anEmitter.chain("some object");
-    }).to.throwError();
   });
 });

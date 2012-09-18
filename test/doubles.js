840ca57aaee8825993c0ca543d04279584b06d09
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-18 16:49:45 +0200

    Green

--- test/doubles.js
+++ test/doubles.js
@@ -7,6 +7,7 @@ module.exports = {
     return {
       yield:sinon.stub(),
       throw:sinon.stub(),
+      done:sinon.stub(),
       chain:sinon.stub()
     };
   },
--- test/internal/is_feed.js
+++ test/internal/is_feed.js
@@ -20,7 +20,7 @@ describe('isFeed():', function () {
 
   ['yield', 'throw', 'done', 'chain'].forEach(objectWithoutThisMethodIsNotAFeed);
 
-  it('an object with "yield", "throw" and "chain" method is a feed', function () {
+  it('an object with "yield", "throw", "done" and "chain" method is a feed', function () {
     expect(utils.isFeed(doubles.makeFeed())).to.be.ok();
   });
 });

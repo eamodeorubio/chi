2aba937e1a5564dba60f7ae9973dca2f2edb64bc
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-18 13:07:23 +0200

    Added unit test for the isFeed check

---
+++ test/internal/is_feed.js
@@ -0,0 +1,26 @@
+"use strict";
+
+var expect = require('expect.js'),
+    doubles = require('../doubles'),
+    utils = require('../../lib/internal/utils');
+
+describe('isFeed():', function () {
+  it('a null is not a feed', function () {
+    expect(utils.isFeed(null)).not.to.be.ok();
+  });
+
+  function objectWithoutThisMethodIsNotAFeed(methodName) {
+    it('an object with no "' + methodName + '" method is not a feed', function () {
+      var almostAFeed = doubles.makeFeed();
+      delete almostAFeed[methodName];
+
+      expect(utils.isFeed(almostAFeed)).not.to.be.ok();
+    });
+  }
+
+  ['yield', 'throw', 'chain'].forEach(objectWithoutThisMethodIsNotAFeed);
+
+  it('an object with "yield", "throw" and "chain" method is a feed', function () {
+    expect(utils.isFeed(doubles.makeFeed())).to.be.ok();
+  });
+});

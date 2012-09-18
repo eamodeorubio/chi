5be14f7dafc97c84135757828f5aeae50e2c346d
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-18 20:34:48 +0200

    Refactored event bus spec. Publish notion is replaced by a more generic notification. There are two types of notifications: 'publish' and 'fire'. The later will not be remembered for future subscribers.

--- test/internal/event_bus.js
+++ test/internal/event_bus.js
@@ -19,79 +19,81 @@ describe('An EventBus:', function () {
     expect(bus.publish).to.be.a('function');
   });
 
-  function knowsHowToPublish(event) {
-    describe('knows how to publish events of type "' + event + '":', function () {
-      describe('given several objects has been subscribed to it', function () {
-        var subscriptor1, subscriptor2, data;
-
-        beforeEach(function () {
-          data = 'passed data';
-
-          subscriptor1 = doubles.double(['x','y','z']);
-          subscriptor2 = doubles.double(['x','y','z']);
-
-          bus.subscribe(subscriptor1);
-          bus.subscribe(subscriptor2);
-        });
-
-        describe('when publishing event ' + event + ' with some data, it', function () {
-          var result;
+  function knowsHowToNotify(notificationType) {
+    return function (event) {
+      describe('knows how to ' + notificationType + ' events of type "' + event + '":', function () {
+        describe('given several objects has been subscribed to it', function () {
+          var subscriptor1, subscriptor2, data;
 
           beforeEach(function () {
-            result = bus.publish(event, data);
+            data = 'passed data';
+
+            subscriptor1 = doubles.double(['x', 'y', 'z']);
+            subscriptor2 = doubles.double(['x', 'y', 'z']);
+
+            bus.subscribe(subscriptor1);
+            bus.subscribe(subscriptor2);
           });
 
-          it('will return nothing', function () {
-            expect(result).to.be(undefined);
+          describe('when ' + notificationType + 'ing an event ' + event + ' with some data, it', function () {
+            var result;
+
+            beforeEach(function () {
+              result = bus.publish(event, data);
+            });
+
+            it('will return nothing', function () {
+              expect(result).to.be(undefined);
+            });
+
+            it('will call the method ' + event + '() on all subscriptors exactly one time', function () {
+              expect(subscriptor1[event].calledOnce).to.be.ok();
+              expect(subscriptor1[event].calledOn(subscriptor1)).to.be.ok();
+
+              expect(subscriptor2[event].calledOnce).to.be.ok();
+              expect(subscriptor2[event].calledOn(subscriptor2)).to.be.ok();
+            });
+
+            it('will pass the same argument to all subscriptors', function () {
+              expect(subscriptor1[event].calledWithExactly(data)).to.be.ok();
+              expect(subscriptor2[event].calledWithExactly(data)).to.be.ok();
+            });
           });
 
-          it('will call the method ' + event + '() on all subscriptors exactly one time', function () {
-            expect(subscriptor1[event].calledOnce).to.be.ok();
-            expect(subscriptor1[event].calledOn(subscriptor1)).to.be.ok();
+          describe('when ' + notificationType + 'ing an event ' + event + ', and one of the subscriptors throws an exception, it', function () {
+            beforeEach(function () {
+              subscriptor1[event].throws();
+            });
 
-            expect(subscriptor2[event].calledOnce).to.be.ok();
-            expect(subscriptor2[event].calledOn(subscriptor2)).to.be.ok();
-          });
+            it('will still call ' + event + '() on the other objects', function () {
+              bus.publish(event, data);
 
-          it('will pass the same argument to all subscriptors', function () {
-            expect(subscriptor1[event].calledWithExactly(data)).to.be.ok();
-            expect(subscriptor2[event].calledWithExactly(data)).to.be.ok();
+              expect(subscriptor2[event].called).to.be.ok();
+            });
           });
         });
 
-        describe('when publishing event ' + event + ', and one of the subscriptors throws an exception, it', function () {
+        describe('given the same object has beed subscribed several times', function () {
+          var aobject;
           beforeEach(function () {
-            subscriptor1[event].throws();
+            aobject = doubles.double(['x', 'y', 'z']);
+
+            bus.subscribe(aobject);
+            bus.subscribe(aobject);
+            bus.subscribe(aobject);
           });
 
-          it('will still call ' + event + '() on the other objects', function () {
-            bus.publish(event, data);
+          it('when ' + notificationType + 'ing an event ' + event + ', it will still call ' + event + '() on the duplicated object exactly one time', function () {
+            bus.publish(event, "not important");
 
-            expect(subscriptor2[event].called).to.be.ok();
+            expect(aobject[event].calledOnce).to.be.ok();
           });
         });
       });
-
-      describe('given the same object has beed subscribed several times', function () {
-        var aobject;
-        beforeEach(function () {
-          aobject = doubles.double(['x','y','z']);
-
-          bus.subscribe(aobject);
-          bus.subscribe(aobject);
-          bus.subscribe(aobject);
-        });
-
-        it('when publishing event ' + event + ', it will still call ' + event + '() on the duplicated object exactly one time', function () {
-          bus.publish(event, "not important");
-
-          expect(aobject[event].calledOnce).to.be.ok();
-        });
-      });
-    });
+    };
   }
 
-  ['x', 'y', 'z'].forEach(knowsHowToPublish);
+  ['x', 'y', 'z'].forEach(knowsHowToNotify('publish'));
 
   describe('given several objects has been subscribed', function () {
     var subscriptor1, subscriptor2;
@@ -129,7 +131,6 @@ describe('An EventBus:', function () {
         expect(subscriptor3.a.calledTwice).to.be.ok();
         expect(subscriptor3.b.calledOnce).to.be.ok();
       });
-
     });
   });
 });

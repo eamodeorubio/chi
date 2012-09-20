995e213e4b08abc4b47dc31ca74be3f9babf7d31
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-20 14:39:04 +0200

    Refactor test(3): Added unit test for success state

---
+++ test/internal/success_state.js
@@ -0,0 +1,36 @@
+"use strict";
+
+var expect = require('expect.js'),
+    doubles = require('../doubles'),
+    feeds = require('../../lib/internal/feeds');
+
+describe('Given a SuccessState,', function () {
+  var state;
+  beforeEach(function () {
+    state = feeds.successState();
+  });
+
+  function cannotDoAnymore(action) {
+    describe('cannot ' + action + '" anymore:', function () {
+      it('it has a ' + action + '() method', function () {
+        expect(state[action]).to.be.a('function');
+      });
+
+      it(action + '() will throw', function () {
+        expect(state[action]).to.throwError();
+      });
+    });
+  }
+
+  ['yield', 'throw'].forEach(cannotDoAnymore);
+
+  describe('can be marked as done:', function () {
+    it('it has a done() method', function () {
+      expect(state.done).to.be.a('function');
+    });
+
+    it('when done is invoked, it only will return itself', function () {
+      expect(state.done()).to.be(state);
+    });
+  });
+});

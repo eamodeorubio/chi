669992681cdd6f1aa80ecc18e529dd891170a346
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-20 14:42:47 +0200

    Refactor test(4): Added unit test for failed state

---
+++ test/internal/failed_state.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var expect = require('expect.js'),
+    doubles = require('../doubles'),
+    feeds = require('../../lib/internal/feeds');
+
+describe('Given a FailedState with an error,', function () {
+  var state, error;
+  beforeEach(function () {
+    error = 'some error';
+    state = feeds.failedState(error);
+  });
+
+  function cannotDoAnymore(action) {
+    describe('cannot ' + action + '" anymore:', function () {
+      it('it has a ' + action + '() method', function () {
+        expect(state[action]).to.be.a('function');
+      });
+
+      it(action + '() will throw', function () {
+        expect(state[action]).to.throwError();
+      });
+    });
+  }
+
+  ['yield', 'throw', 'done'].forEach(cannotDoAnymore);
+
+  it('when throw is invoked with the same error, it only will return itself', function () {
+    expect(state.throw(error)).to.be(state);
+  });
+});

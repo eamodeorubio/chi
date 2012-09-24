66b3b247b87074b1c868f52c32144cc96abb4d84
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-24 13:10:37 +0200

    Plugged factory(1): it will create a bus

--- test/chi/plugins.js
+++ test/chi/plugins.js
@@ -2,6 +2,7 @@
 
 var expect = require('expect.js'),
     doubles = require('./../helpers/doubles'),
+    busModule = require('../../lib/internal/bus'),
     chi = require('../../lib/chi');
 
 describe("The module chi can be extended with plugins:", function () {
@@ -9,7 +10,7 @@ describe("The module chi can be extended with plugins:", function () {
     expect(chi.registerPlugin).to.be.a('function');
   });
 
-  describe("given registerPlugin() has been called with a name and a function", function () {
+  describe("given registerPlugin() has been called with a name and a function,", function () {
     var name, plugin;
     beforeEach(function () {
       name = "plugme";
@@ -18,8 +19,31 @@ describe("The module chi can be extended with plugins:", function () {
       chi.registerPlugin(name, plugin);
     });
 
-    it("a factory method with the same name that the plugin will be available in chi", function () {
+    it("then a factory method with the same name that the plugin will be available in chi", function () {
       expect(chi[name]).to.be.a('function');
+    });
+
+    describe("when the new factory method is called,", function () {
+      var expectedBus;
+
+      beforeEach(function () {
+        expectedBus = doubles.makeBus();
+
+        doubles.stubBusModule(busModule);
+
+        busModule.EventBus.returns(expectedBus);
+      });
+
+      afterEach(function () {
+        busModule.restoreOriginal();
+      });
+
+      it("it will create a new bus", function () {
+        chi[name]();
+
+        expect(busModule.EventBus.calledOnce).to.be.ok();
+        expect(busModule.EventBus.calledWithNew()).to.be.ok();
+      });
     });
   });
 });

fb853eae2c50b10bfe4ea8ac57b215d8fb36199f
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-09-25 17:53:27 +0200

    feeds.registerPlugin will add the plugin in a plugins registry

---
+++ test/internal/feed_plugins.js
@@ -0,0 +1,21 @@
+"use strict";
+
+var expect = require('expect.js'),
+    doubles = require('../helpers/doubles'),
+    feeds = require('../../lib/internal/feeds');
+
+describe('The module internal/feeds can be extended with plugins:', function () {
+  it("has a registerPlugin function", function () {
+    expect(feeds.registerPlugin).to.be.a('function');
+  });
+
+  it("registerPlugin() will add the plugin of a member of feeds.plugins", function () {
+    var name = 'plugin name', plugin = doubles.stubFunction();
+
+    feeds.registerPlugin(name, plugin);
+
+    expect(feeds.plugins).to.be.an('object');
+    expect(feeds.plugins).no.to.be(null);
+    expect(feeds.plugins[name]).to.be(plugin);
+  });
+});

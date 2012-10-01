0c65324bbb27790e55ab8aa5c15c75730eda0a71
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-10-01 17:50:50 +0200

    Extract states from feed to its own package (2): plugins is now private

--- test/internal/states/plugins.js
+++ test/internal/states/plugins.js
@@ -9,16 +9,6 @@ describe('The module internal/states can be extended with plugins:', function ()
     expect(states.registerPlugin).to.be.a('function');
   });
 
-  it("registerPlugin() will add the plugin of a member of feeds.plugins", function () {
-    var name = 'plugin name', plugin = doubles.stubFunction();
-
-    states.registerPlugin(name, plugin);
-
-    expect(states.plugins).to.be.an('object');
-    expect(states.plugins).not.to.be(null);
-    expect(states.plugins[name]).to.be(plugin);
-  });
-
   describe("given a plugin has been registered,", function () {
     var name, plugin;
     beforeEach(function () {

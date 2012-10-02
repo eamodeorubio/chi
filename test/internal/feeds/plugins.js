60de4e6e32234a8af5531e136a7cf4af939e3f5c
Enrique J. Amodeo Rubio <eamodeorubio@gmail.com>
2012-10-02 19:16:25 +0200

    Plugins and feeds (5): redesign, the plugged method will simply call the plugin to create a new feed using the bus factory and the arguments

--- test/internal/feeds/plugins.js
+++ test/internal/feeds/plugins.js
@@ -18,7 +18,7 @@ describe("The module internal/feeds can be extended with plugins:", function () 
       feeds.registerPlugin(name, plugin);
     });
 
-    describe("when a feed is created:", function () {
+    describe("and given a feed has been created:", function () {
       var newFeed, busFactory, stateFactory;
       beforeEach(function () {
         var bus = doubles.makeBus();
@@ -32,24 +32,22 @@ describe("The module internal/feeds can be extended with plugins:", function () 
         newFeed = feeds.feed(busFactory, stateFactory);
       });
 
-      it("it will have a method with the same name that the plugin", function () {
+      it("the new feed will have a method with the same name that the plugin", function () {
         expect(newFeed[name]).to.be.a('function');
       });
 
-      describe("when the pluged method is invoked with some arguments,", function () {
-        var newBus, arg1, arg2;
+      describe("when the plugged method is invoked with some arguments,", function () {
+        var arg1, arg2;
         beforeEach(function () {
           arg1 = "arg1";
           arg2 = "arg2";
 
-          newBus = doubles.makeBus();
-          busFactory.returns(newBus);
-
           newFeed[name](arg1, arg2);
         });
 
-        it("will call the busFactory to create a new bus", function () {
-          expect(busFactory.calledTwice).to.be.ok();
+        it("will call the plugin with the busFactory and the args as an array to create a new feed", function () {
+          expect(plugin.calledOnce).to.be.ok();
+          expect(plugin.calledExactlyWith(busFactory, [arg1, arg2])).to.be.ok();
         });
       });
     });
